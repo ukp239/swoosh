a8c5239d96e7e2932a55374109f5ad0bd10d7c9a
ukp239 <ukp239@gmail.com>
2021-02-28 16:02:43 +0530

    Lesson3

---
+++ Smack/app/src/main/java/com/example/smack/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginCreateUserBtn(view: View){
+
+    }
+
+    fun loginLoginBtnClicked(view: View){
+
+
+    }
+}
